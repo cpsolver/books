b9e75437f4ffea5b1f3bb2dcc6944e819d896977
Richard Fobes <prog@SolutionsCreative.com>
2023-10-27 21:34:18 -0700

    Edit chapters in Goat Spitting Secret audiobook script, and reorganize files to avoid reaching time limit

--- gss/1_expand_libretto.bat
+++ gss/1_expand_libretto.bat
@@ -1,5 +1,6 @@
 
 set PerlPath=F:\Important\Tools\
+set DirExpand=F:\Important\GitHub\books\gss\Expand\
 
 
 rem ---------- verify needed files exist ----------
@@ -9,38 +10,58 @@ rem dir %PerlPath%perl512.dll
 rem dir F:\Important\GitHub\Dashrep-language\dashrep_compiler_executable.pl
 
 
-rem ---------- join ----------
+rem ---------- lengthen compiler loop limit ----------
 
-dir /b /s .\*.txt > list_of_all_gss_files.txt
-copy list_of_all_gss_files.txt temp_list_of_file_paths.txt
-copy list-of-folders-for-generate-collected-dashdef-files.txt temp_list_of_folder_prefixes.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto.txt
+type F:\Important\GitHub\Dashrep-language\dashrep_compiler_executable.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > %DirExpand%dashrep_compiler_executable_longer_limit.pl
 
 
-rem ---------- compile and run ----------
+rem ---------- list files ----------
+
+dir /b /s .\*.txt > %DirExpand%temp_list_of_file_paths.txt
+
+
+rem ---------- move to Expand folder ----------
 
 cd F:\Important\GitHub\books\gss\Expand\
-%PerlPath%perl -w F:\Important\GitHub\Dashrep-language\dashrep_compiler_executable.pl < ..\code_for_compile_expand_libretto.txt > output_log_from_dashrep_compiler.txt
+del output_libretto.html
+
+
+rem ---------- generate first half ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup1\list_of_folder_prefixes.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto.txt > output_log_from_dashrep_compiler.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
-cd F:\Important\GitHub\books\gss\
-%PerlPath%perl F:\Important\GitHub\books\gss\Expand\executable_expand_libretto.pl > F:\Important\GitHub\books\gss\Expand\output_piped_results_from_dashrep_expand_libretto.txt
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_1.txt
+
+
+rem ---------- generate second half and append ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup2\list_of_folder_prefixes.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto.txt > output_log_from_dashrep_compiler.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_1.txt
 
 
 rem ---------- generate the epub version ----------
 
-cd F:\Important\GitHub\books\gss\
-del GoatSpittingSecret.epub
 rem "C:\Program Files\7-Zip\7z.exe" a GoatSpittingSecret.epub .\epub\mimetype
 cd F:\Important\GitHub\books\gss\epub\
 rem "C:\Program Files\7-Zip\7z.exe" a F:\Important\GitHub\books\gss\GoatSpittingSecret.epub
-cd F:\Important\GitHub\books\gss\
 
 
 rem ---------- check HTML tags ----------
 
-%PerlPath%perl %PerlPath%CheckHtmlTags.pl < output_libretto.html > output_check_html_tags.txt
+%PerlPath%perl %PerlPath%CheckHtmlTags.pl < %DirExpand%output_libretto.html > output_check_html_tags.txt
 
 
 pause
