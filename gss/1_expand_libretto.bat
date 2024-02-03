70ed6e1eba0eb36d879a03e8c4d36e286a237065
Richard Fobes <prog@SolutionsCreative.com>
2024-02-03 10:49:55 -0800

    Update batch file to match new distribution among folders

--- gss/1_expand_libretto.bat
+++ gss/1_expand_libretto.bat
@@ -38,71 +38,155 @@ del goat_spitting_secret.html
 rem ---------- generate group 1 ----------
 
 del temp_list_of_folder_prefixes.txt
-copy F:\Important\GitHub\books\gss\LyricsChaptersGroup1\list_of_folder_prefixes_group_1.txt temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup01\list_of_folder_prefixes_group_01.txt temp_list_of_folder_prefixes.txt
 %PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_1.txt
-%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_1.txt > output_log_from_dashrep_compiler_group_1.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_01.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_01.txt > output_log_from_dashrep_compiler_group_01.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
 type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
-%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_1.txt
-copy output_trace.txt output_trace_group_1.txt
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_01.txt
+copy output_trace.txt output_trace_group_01.txt
 
 
 rem ---------- generate group 2 ----------
 
 del temp_list_of_folder_prefixes.txt
-copy F:\Important\GitHub\books\gss\LyricsChaptersGroup2\list_of_folder_prefixes_group_2.txt temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup02\list_of_folder_prefixes_group_02.txt temp_list_of_folder_prefixes.txt
 %PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_2.txt
-%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_2.txt > output_log_from_dashrep_compiler_group_2.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_02.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_02.txt > output_log_from_dashrep_compiler_group_02.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
 type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
-%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_2.txt
-copy output_trace.txt output_trace_group_2.txt
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_02.txt
+copy output_trace.txt output_trace_group_02.txt
 
 
 rem ---------- generate group 3 ----------
 
 del temp_list_of_folder_prefixes.txt
-copy F:\Important\GitHub\books\gss\LyricsChaptersGroup3\list_of_folder_prefixes_group_3.txt temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup03\list_of_folder_prefixes_group_03.txt temp_list_of_folder_prefixes.txt
 %PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_3.txt
-%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_3.txt > output_log_from_dashrep_compiler_group_3.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_03.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_03.txt > output_log_from_dashrep_compiler_group_03.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
 type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
-%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_3.txt
-copy output_trace.txt output_trace_group_3.txt
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_03.txt
+copy output_trace.txt output_trace_group_03.txt
 
 
 rem ---------- generate group 4 ----------
 
 del temp_list_of_folder_prefixes.txt
-copy F:\Important\GitHub\books\gss\LyricsChaptersGroup4\list_of_folder_prefixes_group_4.txt temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup04\list_of_folder_prefixes_group_04.txt temp_list_of_folder_prefixes.txt
 %PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_4.txt
-%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_4.txt > output_log_from_dashrep_compiler_group_4.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_04.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_04.txt > output_log_from_dashrep_compiler_group_04.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
 type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
-%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_4.txt
-copy output_trace.txt output_trace_group_4.txt
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_04.txt
+copy output_trace.txt output_trace_group_04.txt
 
 
 rem ---------- generate group 5 ----------
 
 del temp_list_of_folder_prefixes.txt
-copy F:\Important\GitHub\books\gss\LyricsChaptersGroup5\list_of_folder_prefixes_group_5.txt temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup05\list_of_folder_prefixes_group_05.txt temp_list_of_folder_prefixes.txt
 %PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
-%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_5.txt
-%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_5.txt > output_log_from_dashrep_compiler_group_5.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_05.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_05.txt > output_log_from_dashrep_compiler_group_05.txt
 del executable_expand_libretto.pl
 rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
 type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
-%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_5.txt
-copy output_trace.txt output_trace_group_5.txt
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_05.txt
+copy output_trace.txt output_trace_group_05.txt
+
+
+rem ---------- generate group 6 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup06\list_of_folder_prefixes_group_06.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_06.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_06.txt > output_log_from_dashrep_compiler_group_06.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_06.txt
+copy output_trace.txt output_trace_group_06.txt
+
+
+rem ---------- generate group 7 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup07\list_of_folder_prefixes_group_07.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_07.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_07.txt > output_log_from_dashrep_compiler_group_07.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_07.txt
+copy output_trace.txt output_trace_group_07.txt
+
+
+rem ---------- generate group 8 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup08\list_of_folder_prefixes_group_08.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_08.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_08.txt > output_log_from_dashrep_compiler_group_08.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_08.txt
+copy output_trace.txt output_trace_group_08.txt
+
+
+rem ---------- generate group 9 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup09\list_of_folder_prefixes_group_09.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_09.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_09.txt > output_log_from_dashrep_compiler_group_09.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_09.txt
+copy output_trace.txt output_trace_group_09.txt
+
+
+rem ---------- generate group 10 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup10\list_of_folder_prefixes_group_10.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_10.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_10.txt > output_log_from_dashrep_compiler_group_10.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_10.txt
+copy output_trace.txt output_trace_group_10.txt
+
+
+rem ---------- generate group 11 ----------
+
+del temp_list_of_folder_prefixes.txt
+copy F:\Important\GitHub\books\gss\LyricsChaptersGroup11\list_of_folder_prefixes_group_11.txt temp_list_of_folder_prefixes.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_filter_file_paths.pl > temp_list_of_dashdef_files_to_join.txt
+%PerlPath%perl F:\NewsHereNowLocal\executable_combine_dashdefs.pl > code_for_compile_expand_libretto_group_11.txt
+%PerlPath%perl -w dashrep_compiler_executable_longer_limit.pl < code_for_compile_expand_libretto_group_11.txt > output_log_from_dashrep_compiler_group_11.txt
+del executable_expand_libretto.pl
+rename output_from_dashrep_compiler.pl executable_expand_libretto.pl
+type executable_expand_libretto.pl | %PerlPath%perl -pe "s/900000000/90000000000/" > executable_expand_libretto_longer_limit.pl
+%PerlPath%perl executable_expand_libretto_longer_limit.pl > output_piped_results_from_dashrep_expand_libretto_group_11.txt
+copy output_trace.txt output_trace_group_11.txt
 
 
 rem ---------- generate epub version ----------
